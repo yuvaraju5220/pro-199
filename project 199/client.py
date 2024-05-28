b3876845cf5b4d8163bf69a0fd8f145e3b5de82b
yuvaraju5220 <93331364+yuvaraju5220@users.noreply.github.com>
2024-05-28 09:25:34 +0530

    Add files via upload

---
+++ project 199/client.py
@@ -0,0 +1,13 @@
+import socket
+from threading import Thread
+from tkinter import *
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ip_address = '127.0.0.1'
+port = 8000
+
+client.connect((ip_address, port))
+
+print("Connected with the server...")
+
